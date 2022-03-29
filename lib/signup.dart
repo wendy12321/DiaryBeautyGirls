b0ca358bd656095e9b237af4ec15d313dde0c4b2
Joshua <88637697+JoshuaNope@users.noreply.github.com>
2022-03-29 12:13:20 +0700

    Joshua_Commit

--- lib/signup.dart
+++ lib/signup.dart
@@ -3,8 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:diarybeautygirls/roundedbutton.dart'; 
 
-//code for designing the UI of our text field where the user writes his email id or password
-
 const kTextFieldDecoration = InputDecoration(
   hintText: 'Enter a value',
   hintStyle: TextStyle(color: Colors.grey),
